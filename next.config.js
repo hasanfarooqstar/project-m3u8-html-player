5d44c15b3e71b5b2d512f924a6631e140f47810f
Hassan Farooq <hasanfarooqstar@outlook.com>
2024-12-12 02:52:55 +0100

    vercel json added

--- next.config.js
+++ next.config.js
@@ -1,15 +1,24 @@
 module.exports = {
-    async headers() {
-      return [
-        {
-          source: '/master.m3u8',
-          headers: [
-            { key: 'Access-Control-Allow-Origin', value: '*' },
-            { key: 'Access-Control-Allow-Methods', value: 'GET, OPTIONS' },
-            { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
-          ],
-        },
-      ];
-    },
-  };
-  
+  async headers() {
+    return [
+      {
+        source: '/master.m3u8',
+        headers: [
+          { key: 'Content-Type', value: 'application/vnd.apple.mpegurl' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+        ],
+      },
+      {
+        source: '/(.*).ts',
+        headers: [
+          { key: 'Content-Type', value: 'video/mp2t' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+        ],
+      },
+    ];
+  },
+};
